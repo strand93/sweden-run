80d5c9632c1534d667c73f27e4cc023d919f9e96
strand93 <strand.93@hotmail.com>
2021-01-27 08:56:39 +0100

    Update startup

--- SwedenRun/SwedenRun/Startup.cs
+++ SwedenRun/SwedenRun/Startup.cs
@@ -12,13 +12,11 @@ namespace SwedenRun
 {
   public class Startup
   {
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddControllersWithViews();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
@@ -26,14 +24,13 @@ namespace SwedenRun
         app.UseDeveloperExceptionPage();
       }
 
+      app.UseStatusCodePages();
+      app.UseStaticFiles();
       app.UseRouting();
 
       app.UseEndpoints(endpoints =>
       {
-        endpoints.MapGet("/", async context =>
-              {
-            await context.Response.WriteAsync("Hello World!");
-          });
+        endpoints.MapDefaultControllerRoute();
       });
     }
   }
